35d2bff1e9393d8317cefbe731ea34834978e900
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2020-04-24 23:03:20 +0200

    pydeps

--- tests/pydeps.sh
+++ tests/pydeps.sh
@@ -1,3 +1,4 @@
 # this requires graphviz, which is not python module
 pydeps app -o tmp/dependencies.svg
+# pydeps app --include-missing -o tmp/dependencies_including_missing.svg
 # pydeps app --show-cycles -o tmp/circle_dependencies.svg # this somewhat misbehaves
