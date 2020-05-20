4c61d7134603ccf67cae5be2c1886bcd9a0964c4
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2020-05-20 22:31:36 +0200

    simplify when scan partially fails

--- app/utils/sslyze_result_simplify.py
+++ app/utils/sslyze_result_simplify.py
@@ -1,7 +1,7 @@
 import app.db_models as db_models
 import app.db_schemas as db_schemas
 import app.utils.db_utils as db_utils
-
+from loguru import logger
 
 def sslyze_grade(scan_result: db_models.ScanResults):
     # todo: use grading module
@@ -13,37 +13,41 @@ def sslyze_result_simplify(scan_result: db_models.ScanResults) -> db_models.Scan
     simple = db_models.ScanResultsSimplified()
     simple.id = scan_result.id
     simple.grade = sslyze_grade(scan_result)
-    simple.received_certificate_chain_list_id = scan_result.certificate_information.received_certificate_chain_list_id
 
-    trust_stores = set()
-    verified_chain_list = scan_result.certificate_information.verified_certificate_chain_list
-    received_chain_list = scan_result.certificate_information.received_certificate_chain_list
+    if not scan_result.certificate_information:
+        logger.info(f"Simplifing scan result ({scan_result.id}) which doesn't have certificate_information")
+    else:
+        simple.received_certificate_chain_list_id = scan_result.certificate_information.received_certificate_chain_list_id
 
-    if verified_chain_list:
-        res_new = db_models.db.session \
-            .query(db_models.ValidatedPath) \
-            .filter(db_models.ValidatedPath.chain_id == verified_chain_list.id) \
-            .all()
-        # .filter(db_models.ValidatedPath.chain_id.in_(changed_targets)) \
+        trust_stores = set()
+        verified_chain_list = scan_result.certificate_information.verified_certificate_chain_list
+        received_chain_list = scan_result.certificate_information.received_certificate_chain_list
 
-        for sr in res_new:
-            sr: db_models.ValidatedPath
-            trust_stores.add(sr.trust_store.name)
+        if verified_chain_list:
+            res_new = db_models.db.session \
+                .query(db_models.ValidatedPath) \
+                .filter(db_models.ValidatedPath.chain_id == verified_chain_list.id) \
+                .all()
+            # .filter(db_models.ValidatedPath.chain_id.in_(changed_targets)) \
 
-    simple.validated_against_truststores_list = ", ".join(list(trust_stores))
+            for sr in res_new:
+                sr: db_models.ValidatedPath
+                trust_stores.add(sr.trust_store.name)
 
-    chain_for_dates = verified_chain_list if verified_chain_list else received_chain_list
+        simple.validated_against_truststores_list = ", ".join(list(trust_stores))
 
-    if chain_for_dates:
-        simple.notAfter = db_models.datetime_to_timestamp(chain_for_dates.not_after())
-        simple.notBefore = db_models.datetime_to_timestamp(chain_for_dates.not_before())
+        chain_for_dates = verified_chain_list if verified_chain_list else received_chain_list
 
-    simple.sslv2_working_ciphers_count = len(db_utils.split_array_to_tuple(scan_result.sslv2.accepted_cipher_list))
-    simple.sslv3_working_ciphers_count = len(db_utils.split_array_to_tuple(scan_result.sslv2.accepted_cipher_list))
-    simple.tlsv10_working_ciphers_count = len(db_utils.split_array_to_tuple(scan_result.tlsv1.accepted_cipher_list))
-    simple.tlsv11_working_ciphers_count = len(db_utils.split_array_to_tuple(scan_result.tlsv11.accepted_cipher_list))
-    simple.tlsv12_working_ciphers_count = len(db_utils.split_array_to_tuple(scan_result.tlsv12.accepted_cipher_list))
-    simple.tlsv13_working_ciphers_count = len(db_utils.split_array_to_tuple(scan_result.tlsv13.accepted_cipher_list))
+        if chain_for_dates:
+            simple.notAfter = db_models.datetime_to_timestamp(chain_for_dates.not_after())
+            simple.notBefore = db_models.datetime_to_timestamp(chain_for_dates.not_before())
+
+        simple.sslv2_working_ciphers_count = len(db_utils.split_array_to_tuple(scan_result.sslv2.accepted_cipher_list))
+        simple.sslv3_working_ciphers_count = len(db_utils.split_array_to_tuple(scan_result.sslv2.accepted_cipher_list))
+        simple.tlsv10_working_ciphers_count = len(db_utils.split_array_to_tuple(scan_result.tlsv1.accepted_cipher_list))
+        simple.tlsv11_working_ciphers_count = len(db_utils.split_array_to_tuple(scan_result.tlsv11.accepted_cipher_list))
+        simple.tlsv12_working_ciphers_count = len(db_utils.split_array_to_tuple(scan_result.tlsv12.accepted_cipher_list))
+        simple.tlsv13_working_ciphers_count = len(db_utils.split_array_to_tuple(scan_result.tlsv13.accepted_cipher_list))
 
     # todo: maybe persist to DB?
     return simple
