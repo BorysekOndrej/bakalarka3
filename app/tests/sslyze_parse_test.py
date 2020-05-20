1de47c24561a3c115e1aea60b376325264054a7b
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2020-05-21 00:51:37 +0200

    use general function for testing

--- app/tests/sslyze_parse_test.py
+++ app/tests/sslyze_parse_test.py
@@ -4,6 +4,7 @@ from loguru import logger
 
 from app.utils.files import read_from_file
 from app.utils.sslyze_parse_result import insert_scan_result_into_db
+import app.actions as actions
 
 
 def try_to_insert_all_scan_results():
@@ -14,5 +15,8 @@ def try_to_insert_all_scan_results():
     for filename in os.listdir(path):
         # logger.warning(filename)
         result_string = read_from_file(f'{path}/{filename}')
-        result_dict = json.loads(result_string)
-        insert_scan_result_into_db(result_dict)
+        a = {
+            "results_attached": True,
+            "results": [result_string]
+        }
+        actions.sslyze_send_scan_results(a)
