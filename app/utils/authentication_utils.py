ba60301da68fdb2518f21aa92633d33e9d8706fe
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2020-07-01 16:20:00 +0200

    jwt instance rename in auth util

--- app/utils/authentication_utils.py
+++ app/utils/authentication_utils.py
@@ -1,11 +1,12 @@
+import flask_jwt_extended
 from werkzeug.security import generate_password_hash, check_password_hash
 # werkzeug.security provides salting internally, which is amazing
+
 import app
 import config
 
-jwt = app.jwt
+jwt_instance = app.jwt
 
-import flask_jwt_extended
 
 def hash_password(password: str) -> str:
     return generate_password_hash(str)
@@ -15,7 +16,7 @@ def check_password(known_password_hash: str, password_to_check: str) -> bool:
     return check_password_hash(known_password_hash, password_to_check)
 
 
-@jwt.token_in_blacklist_loader
+@jwt_instance.token_in_blacklist_loader
 def check_if_token_in_blacklist(decrypted_token):
     return False  # todo: token is not blacklisted
 
