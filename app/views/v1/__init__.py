10633aaade72b5f101c257034769ea8e4860cc08
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2020-07-11 14:20:11 +0200

    fix forgotten import

--- app/views/v1/__init__.py
+++ app/views/v1/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 bp = Blueprint('apiV1', __name__)
 
-from . import part1, notification_settings, auth
+from . import misc, notification_settings, auth, sensor_collector
