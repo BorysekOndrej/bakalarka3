992e1add3e040de1f7980f67a81e677725ba96d4
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2020-07-11 20:59:15 +0200

    fix sslyze_save_scan_results_from_obj SEND_RESULTS_OVER_HTTP

--- app/actions/sensor_collector.py
+++ app/actions/sensor_collector.py
@@ -1,25 +1,36 @@
 import jsons
 import requests
 from loguru import logger
+
+import app.utils.files
 import config
 import app.object_models as object_models
 
 
-def sslyze_save_scan_results_from_obj(obj_to_save: object_models.ScanResultResponse) -> bool:
+def sslyze_save_scan_results_from_obj(obj_to_save: object_models.ScanResultResponse, comes_from_http: bool = False)\
+        -> bool:
     if not obj_to_save.results_attached:
         return False
     results = obj_to_save.results
 
+
     if config.SensorCollector.SEND_RESULTS_OVER_HTTP:
-        # todo: do it through app context if it's not sending to collector
+        if comes_from_http:
+            logger.error("Received sslyze scan results to insert but SensorCollector.SEND_RESULTS_OVER_HTTP is enabled here.\n"
+                         "Canceling outbound request, forwarding is not implemented as prevention of endless cycle.")
+        else:
+            # todo: do it through app context if it's not sending to collector
 
-        endpoint_url = f'{config.SensorCollector.BASE_URL}/api/v1/sslyze_import_scan_results'
-        if config.SensorCollector.KEY:
-            endpoint_url += f"/{config.SensorCollector.KEY}"
+            endpoint_url = f'{config.SensorCollector.BASE_URL}/api/v1/sslyze_import_scan_results'
+            if config.SensorCollector.KEY:
+                endpoint_url += f"/{config.SensorCollector.KEY}"
 
-        r = requests.post(endpoint_url, json=jsons.dumps(obj_to_save))
-        print(r.status_code, r.text)
-        # todo: do something with status code
+            dict_to_send = jsons.dump(obj_to_save)
+            # dict_to_send_string = jsons.dumps(obj_to_save)
+            # app.utils.files.write_to_file("tmp/dict_to_send_string3.json", dict_to_send_string)
+            r = requests.post(endpoint_url, json=dict_to_send)
+            print(r.status_code, r.text)
+            # todo: do something with status code
 
     if config.SensorCollector.SEND_RESULTS_TO_LOCAL_DB:
         for single_result in results:
