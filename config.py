1cfa80f62b0a79bb4a6734fb96caab4b95f76cc6
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2020-07-16 22:08:12 +0200

    config clarify job queueu timeout

--- config.py
+++ config.py
@@ -101,6 +101,8 @@ class SchedulerConfig(object):
 
 class SslyzeConfig(object):
     asynchronous_scanning = bool(os.environ.get('SSLYZE_ASYNCHRONOUS_SCANNING', False))
+
+    # The following parameter should be set on main server and currently only works with Redis.
     background_worker_timeout = os.environ.get('SSLYZE_BACKGROUNG_WORKER_TIMEOUT', '3m')  # https://python-rq.org/docs/jobs/
 
     save_results_also_to_tmp_files = True
